83358c468ab9cb027c4c2ae472449928b7ad3613
Agoston Diamont <dagoston93@gmail.com>
2024-04-28 20:32:28 +0200

    Searching implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -27,7 +28,11 @@ function App() {
       paddingX={5}
     >
       <GridItem area="nav">
-        <NavBar />
+        <NavBar
+          onSearch={(searchText) =>
+            setGameQuery({ ...gameQuery, searchText: searchText })
+          }
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside">
