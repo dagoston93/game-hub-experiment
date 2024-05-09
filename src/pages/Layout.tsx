ecf08d4e100541fa6ee80817657a79df8de1b079
Agoston Diamont <dagoston93@gmail.com>
2024-05-10 00:13:30 +0200

    Fetching game details implemented.

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,13 @@
+import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const Layout = () => {
   return (
-    <>
+    <Box padding={5}>
       <NavBar />
       <Outlet />
-    </>
+    </Box>
   );
 };
 
